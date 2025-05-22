03a2b3471fdd69f63875aceb3c1a58215deda73e
Ganbusier <953133083@qq.com>
2025-05-22 16:29:13 +0200

    rewrite the cgal cylinder class to add radius range parameters

--- src/ransac/customCylinder.h
+++ src/ransac/customCylinder.h
@@ -4,72 +4,46 @@
 #include <CGAL/Shape_detection/Efficient_RANSAC/Cylinder.h>
 
 namespace ransac {
-    class CustomCylinder : public Cylinder {
-        private:
-            FT m_radius;
-            Point_3 m_point_on_axis;
-            Line_3 m_axis;
-            bool m_wrap_u;
-            FT m_minRadius = 0.001;
-            FT m_maxRadius = 1.0;
+template <class Traits>
+class CustomCylinder : public CGAL::Shape_detection::Cylinder<Traits> {
+ public:
+  using Base = CGAL::Shape_detection::Cylinder<Traits>;
+  using FT = typename Traits::FT;
 
-        public:
-            void setRadiusRange(FT minRadius, FT maxRadius) {
-                m_minRadius = minRadius;
-                m_maxRadius = maxRadius;
-            }
+  CustomCylinder()
+      : Base(), m_min_radius(0), m_max_radius(std::numeric_limits<FT>::max()) {}
 
-        protected:
-            void create_shape(const std::vector<std::size_t> &indices) override {
-                Point_3 p1 = this->point(indices[0]);
-                Point_3 p2 = this->point(indices[1]);
+  // set radius range
+  void set_radius_range(FT minRadius, FT maxRadius) {
+    m_minRadius = minRadius;
+    m_maxRadius = maxRadius;
+  }
 
-                Vector_3 n1 = this->normal(indices[0]);
-                Vector_3 n2 = this->normal(indices[1]);
+  // get radius range
+  FT min_radius() const { return m_minRadius; }
+  FT max_radius() const { return m_maxRadius; }
 
-                Vector_3 axis = this->cross_pdct(n1, n2);
-                FT axisL = CGAL::sqrt(this->sqlen(axis));
-                if (axisL < (FT)0.0001) {
-                  return;
-                }
-                axis = this->scale(axis, FT(1.0) / axisL);
+ protected:
+  // override create_shape function, add radius range check
+  virtual void create_shape(const std::vector<std::size_t>& indices) override {
+    // call base class create_shape function
+    Base::create_shape(indices);
 
-                // establish two directions in the plane axis * x = 0,
-                // whereas xDir is the projected n1
-                Vector_3 xDir = this->sum_vectors(
-                  n1, this->scale(axis, -this->scalar_pdct(n1, axis)));
-                xDir = this->scale(xDir, (FT)1.0 / CGAL::sqrt(this->sqlen(xDir)));
-                Vector_3 yDir = this->cross_pdct(axis, xDir);
-                yDir = this->scale(yDir, (FT)1.0 / CGAL::sqrt(this->sqlen(yDir)));
+    // if base class create failed, return
+    if (!this->m_is_valid) {
+      return;
+    }
 
-                FT n2x = this->scalar_pdct(n2, yDir);
-                FT n2y = -this->scalar_pdct(n2, xDir);
+    // check if radius is in range
+    if (this->radius() < m_minRadius || this->radius() > m_maxRadius) {
+      this->m_is_valid = false;
+    }
+  }
 
-                Vector_3 dist = this->constr_vec(p1, p2);
+ private:
+  FT m_minRadius;  // minimum radius
+  FT m_maxRadius;  // maximum radius
+};
+}  // namespace ransac
 
-                FT Ox = this->scalar_pdct(xDir, dist);
-                FT Oy = this->scalar_pdct(yDir, dist);
-
-                FT lineDist = n2x * Ox + n2y * Oy;
-
-                m_radius = lineDist / n2x;
-                m_point_on_axis = this->transl(p1, this->scale(xDir, m_radius));
-                m_radius = CGAL::abs(m_radius);
-
-                m_axis = this->m_traits.construct_line_3_object()(m_point_on_axis, axis);
-
-                if (squared_distance(p1) > this->m_epsilon ||
-                    (cos_to_normal(p1, n1) < this->m_normal_threshold) ||
-                    m_radius < m_minRadius || m_radius > m_maxRadius)
-                  return;
-
-                this->m_is_valid = true;
-                this->m_wrap_u = true;
-            }
-    };
-}
-
-
-
-#endif // CUSTOM_CYLINDER_H
-
+#endif  // CUSTOM_CYLINDER_H
